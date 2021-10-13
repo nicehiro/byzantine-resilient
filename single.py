7b62827f9a7b6ab33d4ed9e1af1762d7de3ce8c8
hiro <wfy11235813@gmail.com>
2021-10-13 14:08:36 +0800

    /home/hiro/Documents/byzantine-resilient/venv/bin/python

--- single.py
+++ single.py
@@ -10,15 +10,15 @@ adj_matrix = [[1]]
 attacks = [None]
 
 
-def train():
-    data_distributor = DataDistributor("data", "MNIST", 64, 1)
+def train(dataset):
+    data_distributor = DataDistributor("data", dataset, 64, 1)
     data_distributor.distribute()
     train_loaders = data_distributor.train_loaders
     test_loader = data_distributor.test_loader
     worker = Worker(
         0, meta_lr=1e-3, gar=average, attack=None, criterion=F.cross_entropy
     )
-    worker.set_dataset("MNIST", train_loaders[0], test_loader)
+    worker.set_dataset(dataset, train_loaders[0], test_loader)
     # worker.reset_meta_model()
 
     for epoch in range(10000):
@@ -28,13 +28,13 @@ def train():
         flat_params -= worker.meta_lr * grad
         worker.set_meta_model_flat_params(flat_params)
         if epoch % 10 == 0:
+            acc = worker.meta_test()
             print(
-                "Train Epoch: {} \tLoss: {:.6f}".format(
-                    epoch,
-                    loss.item(),
+                "Train Epoch: {} \tLoss: {:.6f} \tAcc: {}".format(
+                    epoch, loss.item(), acc
                 )
             )
 
 
 if __name__ == "__main__":
-    train()
+    train("CIFAR")
