bug fix: dist.sendd and dist.recv have bugs for looped graph, so I use dist.broadcast and dist.gather

worker.py
@@ -18,7 +18,7 @@ class Worker(Process):
     meta_models = {"MNIST": MNIST, "CIFAR10": CIFAR10}
     # grad_shape = {"MNIST": (25450, 1), "CIFAR10": (62006, 1)}
     MASTER_ADDR = "127.0.0.1"
-    MASTER_PORT = "29600"
+    MASTER_PORT = "29901"
 
     def __init__(
         self,
@@ -64,8 +64,11 @@ class Worker(Process):
         os.environ["MASTER_ADDR"] = self.MASTER_ADDR
         os.environ["MASTER_PORT"] = self.MASTER_PORT
         dist.init_process_group(backend="gloo", rank=self.rank, world_size=self.size)
-        # src_group = dist.new_group(self.src)
-        # dst_group = dist.new_group(self.dst)
+        dst_group, src_group = None, None
+        if len(self.dst) != 0:
+            dst_group = dist.new_group(self.dst)
+        if len(self.src) != 0:
+            src_group = dist.new_group(self.src)
         for epoch in range(self.epochs):
             if self.rank in self.test_ranks:
                 acc = self.meta_test()
@@ -82,14 +85,16 @@ class Worker(Process):
                 # contain all grads received from other worker
                 grads = [torch.zeros_like(grad)] * len(self.src)
                 # send/recv grads to/from neighbors
-                for d in self.dst:
-                    dist.send(tensor=grad, dst=d)
-                    logging.info(f"Rank {self.rank} send grad to {d}")
-                for i, s in enumerate(self.src):
-                    dist.recv(tensor=grads[i], src=s)
-                    logging.info(f"Rank {self.rank} receive grad from {s}")
-                # dist.broadcast(grad, self.rank, dst_group)
-                # dist.gather(grad, grads, self.rank, src_group)
+                # for d in self.dst:
+                #     dist.send(tensor=grad, dst=d)
+                #     logging.info(f"Rank {self.rank} send grad to {d}")
+                # for i, s in enumerate(self.src):
+                #     dist.recv(tensor=grads[i], src=s)
+                #     logging.info(f"Rank {self.rank} receive grad from {s}")
+                if dst_group is not None:
+                    dist.broadcast(grad, self.rank, dst_group)
+                if src_group is not None:
+                    dist.gather(grad, grads, self.rank, src_group)
                 grad = self.gar(grads + [grad])
                 set_grads(self.meta_model, grad)
                 self.optimizer.step()