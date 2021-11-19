7763dae8375d45d8eb6ef24e48698f9eb6606dca
hiro <wfy11235813@gmail.com>
2021-11-19 09:59:39 +0800

    add guassian attack

---
+++ attack/guassian_attack.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from attack.attack import Attack
+
+import torch
+
+
+class GuassianAttack(Attack):
+    """
+    Gaussian Attack.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def attack(self, params_list: List[torch.Tensor]):
+        all_params = torch.stack(params_list, dim=1)
+        # get mean and std
+        mean_params = all_params.mean(dim=1)
+        std_params = all_params.std(dim=1)
+        # gaussian sample between min and max
+        res = torch.normal(mean_params, std_params)
+        return res
