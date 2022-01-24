ba278b5b976c0eb339e14a17771f61bb013fe1c4
hiro <wfy11235813@gmail.com>
2022-01-24 10:42:05 +0800

    add script to train all

---
+++ all_batch_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+attacks=("max" "gaussian" "hidden" "litter" "empire")
+
+for attack in ${attacks[@]}
+do
+    bash batch_train.sh -d MNIST -b 0.1 -c 0.5 -a $attack
+
+    if [ $? -ne 0 ]
+    then
+        echo "Exit"
+        exit 1
+    fi
+    echo $(date +"%m-%d %H:%M") ".........$connection_ratio/$byzantine_ratio: $attack-$par Done.........."
+done
